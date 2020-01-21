6eb3ae197f7341a629f8b6eea0e8d8ad17e57186
Parker Okonek <parkerokonek@gmail.com>
2020-01-20 22:44:11 -0800

    Set up CMake and Qt for some Qt testing

---
+++ README.md
@@ -0,0 +1,13 @@
+# Compilation
+building requires:
+* Cmake
+* make
+* Qt5Widgets library
+* g++ with C++17 compatibility 
+
+## To build
+```
+cd to/project/directory
+cmake .
+make
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
-// Flawless, perfect, no error application
-int main() {
-    return 0;
+#include "mainwindow.hpp"
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    Ui_MainWindow ui = Ui_MainWindow();
+    QMainWindow window = QMainWindow();
+    
+    ui.setupUi(&window);
+
+    window.show();
+    
+    return app.exec();
 }
